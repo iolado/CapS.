6daf5c5be0b9117ae9d94a7fa85658f7d20a809d
iolado <oladokui@oregonstate.edu>
2026-04-20 18:10:35 -0700

    env created

--- server/db/client.js
+++ server/db/client.js
@@ -4,9 +4,9 @@ import pg from "pg";
 // This creates a variable called url.
 const url =
   // This first checks if there is a DATABASE_URL in the environment.
-  process.env.DATABASE_URL ||
+  process.env.DATABASE_URL
   // If there is no DATABASE_URL, it uses this local database connection string instead.
-  "postgres://ifeoladokun:password@localhost:5432/skill_tree_builder";
+  // "postgres://ifeoladokun:password@localhost:5432/skill_tree_builder";
 
 // This creates a new PostgreSQL client using the url above.
 const db = new pg.Client(url);
